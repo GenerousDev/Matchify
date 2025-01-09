a33ce6df4577bec3a66999e7b6d35ba87f82632c
GenerousDev <omolaja.abubakar05@gmail.com>
2025-01-09 16:56:31 +0100

    added npm start to yml file

--- matchify-frontend/wdio.conf.js
+++ matchify-frontend/wdio.conf.js
@@ -50,7 +50,10 @@ exports.config = {
     // https://saucelabs.com/platform/platform-configurator
     //
     capabilities: [{
-        browserName: 'chrome'
+        browserName: 'chrome',
+        'goog:chromeOptions': {
+            args: ['--headless', '--disable-gpu', '--window-size=1920x1080']
+          }
     }],
 
     //
